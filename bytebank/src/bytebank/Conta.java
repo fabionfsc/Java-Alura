be849d1a2f897543de07fd07666ab29dd10fba19
fabio.ferreirai <fabio.ferreirai@CTIS30689SPBW10.sonda.com>
2023-08-08 20:59:20 -0300

    bytebank atualizado

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -1,5 +1,30 @@
 package bytebank;
 
 public class Conta {
+	int agencia;
+	int conta;
+	double saldo;
+	String titular;
 
+	public void deposita(double valor) {
+		saldo = saldo + valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			saldo = saldo - valor;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (valor >= saldo) {
+			saldo = saldo - valor;
+			destino.saldo = saldo + valor;
+			return true;
+		}
+		return false;
+
+	}
 }
--- bytebank/src/bytebank/TestaMetodos.java
+++ bytebank/src/bytebank/TestaMetodos.java
@@ -1,5 +1,17 @@
 package bytebank;
 
 public class TestaMetodos {
-
+	public static void main(String[] args) {
+		Conta contaDoFabio = new Conta();
+		
+		contaDoFabio.deposita(1000);
+		System.out.println("Seu saldo é de " + contaDoFabio.saldo);
+		
+		contaDoFabio.saca(900);
+		System.out.println(contaDoFabio.saldo);
+		
+		Conta contaDoIgor = new Conta();
+		
+		conta
+	}
 }
