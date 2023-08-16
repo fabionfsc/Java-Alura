a86ec4ca964a986485d2d556e0059d856dcaa270
PA 19 <fabio.ferreirai@sonda.com>
2023-08-16 19:19:19 -0300

    commit 16/08

---
+++ bytebankherdado/src/bytebankherdado/EditorVideo.java
@@ -0,0 +1,9 @@
+package bytebankherdado;
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao() {
+		return super.getBonificacao() + 100;
+	}
+	
+}
--- bytebankherdado/src/bytebankherdado/TesteReferencias.java
+++ bytebankherdado/src/bytebankherdado/TesteReferencias.java
@@ -3,12 +3,26 @@ package bytebankherdado;
 public class TesteReferencias {
 	public static void main(String[] args) {
 		
-		Funcionario g1 = new Gerente();
-		g1.setNome("Igor");
+		Funcionario g = new Gerente();
+		g.setNome("Igor");
+		g.setSalario(5000.0);
 		
-		String showname = g1.getNome();
+		Funcionario f = new Funcionario();
+		f.setSalario(1000);
 		
-		System.out.println(showname);
+		Funcionario e = new EditorVideo();
+		e.setSalario(2000);
+		
+	
+		ControleBonificacao controle = new ControleBonificacao();
+		
+		controle.registra(g);
+		controle.registra(f);
+		controle.registra(e);
+		
+		System.out.println(controle.getSoma());
+		
+		
 		
 	}
 
