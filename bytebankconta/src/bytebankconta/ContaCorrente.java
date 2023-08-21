60a05260e86999b3675625a18e34a4c34f537420
fabio.ferreirai <fabio.ferreirai@CTIS30616SPBW10.sonda.com>
2023-08-21 19:58:04 -0300

    commit 21/08

--- bytebankconta/src/bytebankconta/ContaCorrente.java
+++ bytebankconta/src/bytebankconta/ContaCorrente.java
@@ -1,6 +1,6 @@
 package bytebankconta;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -16,5 +16,10 @@ public class ContaCorrente extends Conta {
 	public void deposita(double valor) {
 		super.saldo += valor;
 		
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
 	}
 }
