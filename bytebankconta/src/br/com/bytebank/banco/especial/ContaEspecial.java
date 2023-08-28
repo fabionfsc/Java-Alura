f5044c0cef2b32b4fef3f18dd1c77ca9ab58db7f
fabio.ferreirai <fabio.ferreirai@CTIS30780SPBW10.sonda.com>
2023-08-28 19:53:55 -0300

    commit 28/08

--- bytebankconta/src/br/com/bytebank/banco/especial/ContaEspecial.java
+++ bytebankconta/src/br/com/bytebank/banco/especial/ContaEspecial.java
@@ -6,13 +6,11 @@ public class ContaEspecial extends Conta {
 
 	public ContaEspecial(int agencia, int numero) {
 		super(agencia, numero);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void deposita(double valor) {
-		// TODO Auto-generated method stub
+		super.saldo += valor;
 
 	}
-
 }
