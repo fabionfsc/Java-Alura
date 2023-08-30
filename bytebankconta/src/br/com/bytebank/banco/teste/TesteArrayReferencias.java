aff5a6e0abf4e037bdcdfff97f5f89c5bb082c5b
PA 19 <fabio.ferreirai@sonda.com>
2023-08-30 19:20:46 -0300

    commit 30/08

--- bytebankconta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
+++ bytebankconta/src/br/com/bytebank/banco/teste/TesteArrayReferencias.java
@@ -1,8 +1,36 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
+import br.com.bytebank.banco.modelo.Conta;
+
 public class TesteArrayReferencias {
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws SaldoInsuficienteException {
+		
+		
+		Conta[] contas = new ContaCorrente[5];
+		ContaCorrente cc1 = new ContaCorrente(111, 222);
+		
+		contas[0] = cc1;
+		
+		contas[0].deposita(1000);
+		
+		System.out.println(contas[0].getSaldo());
+		
+		ContaCorrente cc2 = new ContaCorrente(111, 333);
+		
+		contas[1] = cc2;
+		
+		contas[1].deposita(1500);
+		
+		contas[1].transfere(1000, contas[0]);
+		
+		System.out.println(contas[1].getSaldo());
+		
+		contas[2] = new ContaCorrente(10, 22);
+		
+		System.out.println(contas[2].getAgencia());
 		
 	}
 
