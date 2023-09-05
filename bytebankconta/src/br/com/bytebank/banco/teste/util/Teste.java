21e8725ccc7d3fa89284b4e00a23bc232f7c6a4a
fabio.ferreirai <fabio.ferreirai@CTIS30780SPBW10.sonda.com>
2023-09-05 19:11:20 -0300

    commit 05/09

--- bytebankconta/src/br/com/bytebank/banco/teste/util/Teste.java
+++ bytebankconta/src/br/com/bytebank/banco/teste/util/Teste.java
@@ -2,12 +2,32 @@ package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
 
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList lista = new ArrayList();
 
+		Conta ccref = new ContaCorrente(100, 200);
+		lista.add(ccref);
+
+		Conta ccref2 = new ContaCorrente(101, 201);
+		lista.add(ccref2);
+
+		System.out.println(lista.size());
+
+		// For Clássico
+		for (int i = 0; i < lista.size(); i++) {
+			System.out.println(lista.get(i));
+		}
+
+		// For Simplificado
+		for (Object oRef : lista) {
+			System.out.println(oRef);
+		}
 	}
 
 }
